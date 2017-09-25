ec953ddb4ad01131c0ba004e973cc7ad17daac01
Cezar Pretto <operador@cezar.planos>
2017-09-25 10:31:18 -0300

    Changin the way how the project folder is integrated with the project

--- configs.js
+++ configs.js
@@ -1,51 +1,7 @@
-const path = `${process.cwd()}/projects`
-const configs = [
-  {
-    name: 'api-gateway',
-    location: `${path}/api-gateway`,
-    dev: {
-      imageName: 'api-gateway-dev:0.0.1',
-      containerName: 'api-gateway-dev',
-      options: {
-        memory: '512m',
-        port: '80:80',
-        cpus: '.25',
-        volume: `${path}/api-gateway/dev/dev.conf:/etc/nginx/conf.d/default.conf`
-      }
-    },
-    prod: {
-      imageName: 'api-gateway:0.0.1',
-      containerName: 'api-gateway',
-      options: {
-        memory: '512m',
-        port: '80:80',
-        cpus: '.25'
-      }
-    }
-  },
-  {
-    name: 'funcionario-service',
-    location: `${path}/funcionario-service`,
-    dev: {
-      imageName: 'funcionario-service-dev:0.0.1',
-      containerName: 'funcionario-service-dev',
-      options: {
-        memory: '512m',
-        port: '3001:3000',
-        cpus: '.25',
-        volume: '/home/operador/Documents/projetos/web/planos/funcionario-service:/home/node/funcionario-service'
-      }
-    },
-    prod: {
-      imageName: 'funcionario-service:0.0.1',
-      containerName: 'funcionario-service',
-      options: {
-        memory: '512m',
-        port: '3001:3000',
-        cpus: '.25'
-      }
-    }
-  }
-]
+const configs = {
+  configFile: '/home/operador/Documents/projetos/web/planos/docker/projects/configs.js'
+}
 
-module.exports = configs
+const configFile = require(configs.configFile)
+
+module.exports = configFile
--- projects/funcionario-service/dev/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cat > /etc/resolv.conf <<EOT
-nameserver 172.16.10.101
-search planos.net
-EOT
